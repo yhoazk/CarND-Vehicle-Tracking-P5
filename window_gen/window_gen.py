7466a6352bac50abb1c04c49b47de9f8aaa24a06
Gustavo Espindola <yhoazk@gmail.com>
2017-02-20 13:52:59 -0600

    Fixed bug in order of window

--- window_gen/window_gen.py
+++ window_gen/window_gen.py
@@ -6,14 +6,18 @@ import cv2
 from matplotlib.animation import FuncAnimation
 def __draw_rect(img, bboxes, color=(0, 0, 255), thick=6):
     # Make a copy of the image
-    imcopy = np.copy(img)
     # Iterate through the bounding boxes
-    for bbox in bboxes:
+    imcopy = np.copy(img)
+    for n,bbox in enumerate(bboxes):
         # Draw a rectangle given bbox coordinates
         cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
-        plt.imshow(imcopy)
-        plt.show()
+        cv2.imshow("_", imcopy)
+        section = imcopy[bbox[0][1]:bbox[1][1],bbox[0][0]:bbox[1][0]]
+        print(str(section.shape) + "::" + str(bbox[0][0])+":"+ str(bbox[1][0])+","+ str(bbox[0][1])+":"+ str(bbox[1][1]))
+        cv2.imwrite("sample_"+str(n)+".png", section)
+        cv2.waitKey(200)
     # Return the image copy with boxes drawn
+    cv2.destroyAllWindows()
     return imcopy
 def draw(img, rect):
     pass
@@ -21,16 +25,17 @@ def draw(img, rect):
 
 
 
-def gen_windows(img, x_s_t=[None, None], y_s_t=[None,None], xy_w=(32,32), xy_overlap=(0.4,0.4),sweep=7):
-    if None in x_s_t or None in y_s_t:
+def gen_windows(img, x_s_t=[None, None], y_s_t=[None,None], xy_w=(32,32), xy_overlap=(0.4,0.7),sweep=7):
+    if None in x_s_t:
         x_s_t = [0, img.shape[1]]
+    if None in y_s_t:
         y_s_t = [0, img.shape[0]]
 
     x_range = x_s_t[1] - x_s_t[0]
     y_range = y_s_t[1] - y_s_t[0]
 
     #define window sizes
-    w_sizes = np.linspace(32,140,7, dtype="int32") #array([  32.,   48.,   64.,   80.,   96.,  112.,  128.]) 
+    w_sizes = np.linspace(32,270,6, dtype="int32") #array([  32.,   48.,   64.,   80.,   96.,  112.,  128.])
     #  given the overlap find the y start points
     y_start_points = [y_s_t[0]]
     y_start_points.extend([y_s_t[0]+(1.0-xy_overlap[1])*x for x in w_sizes])
@@ -51,7 +56,7 @@ def gen_windows(img, x_s_t=[None, None], y_s_t=[None,None], xy_w=(32,32), xy_ove
 
 if __name__ == '__main__':
     img = plt.imread("sample.jpg")
-    w = gen_windows(img, [80, 1200], [375, 700])
+    w = gen_windows(img, y_s_t= [380, 700])
     f1,f2= w[0]
     print("-"*20)
     sec = img[f1[0]:f2[0],f1[1]:f2[1]]
